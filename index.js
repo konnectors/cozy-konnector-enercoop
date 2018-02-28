245f9d08e1a324a05b2332ea457cc2167822a227
doubleface <christophe@cozycloud.cc>
2018-02-28 09:32:30 +0100

    publish: fix: webpack build output file
    generated from commit 6ff3ebbc9eed12795647e24b96653b8a416e0f7a

--- index.js
+++ index.js
@@ -55839,7 +55839,6 @@ function parsePage ($) {
 
     //gets pdf download URL
     let pdfUrl = $(this).find('a > i').data('url')
-    pdfUrl = baseUrl + pdfUrl
 
     //<French month>-YYYY format (Décembre - 2017)
     let billDate = $(this).find('.invoiceDate').text().trim()
@@ -55855,8 +55854,13 @@ function parsePage ($) {
       amount,
       date: date.toDate(),
       vendor: 'Enercoop',
-      filename: `${date.format('YYYYMM')}_enercoop.pdf`,
-      fileurl: pdfUrl
+    }
+
+    if (pdfUrl) {
+        Object.assign(bill, {
+            filename: `${date.format('YYYYMM')}_enercoop.pdf`,
+            fileurl: baseUrl + pdfUrl
+        })
     }
 
     bills.push(bill)
